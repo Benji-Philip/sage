6bbe2783ec8a473f3553c7d86b0bcef891cda546
Benji-Philip <99130709+Benji-Philip@users.noreply.github.com>
2024-09-12 11:22:55 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:sage/theme/theme.dart';
+import 'package:sage/pages/home_page.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: lightMode,
+      darkTheme: darkMode,
+    );
+  }
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: const ColorScheme.light(
+    background: Colors.white,
+    onPrimary: Color.fromARGB(255, 255, 255, 255),
+    primary: Colors.black,
+    secondary: Color.fromARGB(255, 199, 199, 199),
+    onSecondary: Color.fromARGB(255, 235, 235, 235),
+    tertiary: Colors.black,
+    onTertiary: Color.fromARGB(255, 188, 188, 188),
+  ), // ColorScheme.light
+); // ThemeData
+
+ThemeData darkMode = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: const ColorScheme.dark(
+    background: Colors.black,
+    onPrimary: Color.fromARGB(255, 20, 20, 20),
+    primary: Colors.white,
+    secondary: Color.fromARGB(255, 26, 26, 26),
+    onSecondary: Color.fromARGB(255, 40, 40, 40),
+    tertiary: Color.fromARGB(255, 49, 49, 49),
+    onTertiary: Color.fromARGB(255, 50, 50, 50),
+  ), // ColorScheme.dark
+); // ThemeData
